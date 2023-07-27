ab74cfea3fa8b694e4db3ab311ce7c19ac83af2e
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2023-07-27 12:02:55 -0600

    stuff

---
+++ Products/Entries/2023/7/27_LED_Matrix_files/27_LED_Matrix.js
@@ -0,0 +1,16 @@
+// Created by iWeb 3.0.4 local-build-20230727
+
+function writeMovie1()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id4" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="160" height="300" style="height: 300px; left: 270px; position: absolute; top: 20px; width: 160px; z-index: 1; "><param name="src" value="../../../../Media/IMG_7718.MOV" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id4" type="video/quicktime" width="160" height="300" style="height: 300px; left: 270px; position: absolute; top: 20px; width: 160px; z-index: 1; "><param name="src" value="27_LED_Matrix_files/IMG_7718.jpg"/><param name="target" value="myself"/><param name="href" value="../../../../../Media/IMG_7718.MOV"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id4" type="video/quicktime" width="160" height="300" data="../../../../Media/IMG_7718.MOV" style="height: 300px; left: 270px; position: absolute; top: 20px; width: 160px; z-index: 1; "><param name="src" value="../../../../Media/IMG_7718.MOV"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('27_LED_Matrix_files/27_LED_MatrixMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Products/Products_files/Products.js
@@ -0,0 +1,13 @@
+// Created by iWeb 3.0.4 local-build-20230727
+
+setTransparentGifURL('../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("file://localhost/Users/Griffin/Desktop/Site/Products/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Products_files/ProductsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupAllIEPNGBGs();fixupIECSS3Opacity('id3');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
